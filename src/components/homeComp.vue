fd2b07b432a5ea28112914b2014ac9f86a2ff85d
mark <ngarimark27@gmail.com>
2024-09-25 14:27:05 +0300

    added   pic

--- src/components/homeComp.vue
+++ src/components/homeComp.vue
@@ -2,21 +2,21 @@
 <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active" data-bs-interval="10000">
-      <img src="../pics/services-img-02.jpg" class="d-block" style="width: 100vw; height: 90vh;" alt="...">
+      <img src="../pics/download.jpeg" class="d-block" style="width: 100vw; height: 90vh;" alt="...">
       <div class="carousel-caption d-none d-md-block">
         <h5>our expertise</h5>
         <p>Experience the ultimate car wash experience - clean cars, happy drivers!</p>
       </div>
     </div>
     <div class="carousel-item" data-bs-interval="2000">
-      <img src="../pics/services-img-03.jpg" class="d-block" style="width: 100vw; height: 90vh;" alt="...">
+      <img src="../pics/download (1).jpeg" class="d-block" style="width: 100vw; height: 90vh;" alt="...">
       <div class="carousel-caption d-none d-md-block">
         <h5>our efficiency</h5>
         <p>Quick, easy, and convenient car washing - book online now</p>
       </div>
     </div>
     <div class="carousel-item">
-      <img src="../pics/services-img-04.jpg" class="d-block" style="width: 100vw; height: 90vh;" alt="...">
+      <img src="../pics/download (2).jpeg" class="d-block" style="width: 100vw; height: 90vh;" alt="...">
       <div class="carousel-caption d-none d-md-block">
         <h5>our service</h5>
         <p>Premium car wash service that leaves your vehicle looking showroom new</p>
@@ -48,6 +48,52 @@
   <small>15% off your next service</small>
   <button class="btn btn-success mt-2">Claim Offer</button>
 </div>
+<section id="testimonials" class="py-5 bg-white">
+  <h2 class="text-center mb-4 text-black">What Our Customers Say</h2>
+  <div class="row row-cols-1 row-cols-md-2 g-4">
+    <div class="col">
+      <div class="card bg-secondary text-white">
+        <div class="card-body">
+          <blockquote class="blockquote">
+            <p>"🚗💨 Their eco-friendly wash left my vehicle spotless without harsh chemicals!"</p>
+            <footer class="blockquote-footer text-white">Sarah K.</footer>
+          </blockquote>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card bg-secondary text-white">
+        <div class="card-body">
+          <blockquote class="blockquote">
+            <p>"The quick turnaround time allowed me to run errands while my car was being serviced. Highly recommended!"</p>
+            <footer class="blockquote-footer text-white">David M.</footer>
+          </blockquote>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card bg-secondary text-white">
+        <div class="card-body">
+          <blockquote class="blockquote">
+            <p>"Their attention to detail is impressive. They even polished the wheels to a shine!"</p>
+            <footer class="blockquote-footer text-white">Emily T.</footer>
+          </blockquote>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card bg-secondary text-white">
+        <div class="card-body">
+          <blockquote class="blockquote">
+            <p>"The interior detailing was so thorough, I couldn't believe how clean it got! Worth every penny."</p>
+            <footer class="blockquote-footer text-white">James B.</footer>
+          </blockquote>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
 
 </template>
 
